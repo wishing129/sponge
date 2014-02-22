37a08e604ae1f52df495a033920ebebb5ae69744
jinghanx <jinghanx@gmail.com>
2014-02-22 12:14:55 -0800

    check in the app folder

---
+++ TimeInSponge/TimeInSponge/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TimeInSponge
+//
+//  Created by Jinghan Xu on 2/22/14.
+//  Copyright (c) 2014 TimeInSponge.inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
