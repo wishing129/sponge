782561e4613770456623ccdf14eab41638293633
wishing129 <weixin0129@gmail.com>
2014-02-23 18:38:06 -0800

    access by repositories

--- timeinsponge/src/main/java/com/timeinsponge/meeting/object/Event.java
+++ timeinsponge/src/main/java/com/timeinsponge/meeting/object/Event.java
@@ -1,21 +1,100 @@
 package com.timeinsponge.meeting.object;
 
+import java.util.List;
+
 public class Event {
 	
-	public String eventId;
-	public String owner;
-	public String title;
-	public String description;
-	public String location;
+	private String eventId;
+	private EventType type;
+	private User owner;
+	private String title;
+	private String description;
+	private String location;
+	private List<EventUser> participants;
 	
-	static public Event getAnEvent() {
-		Event e = new Event();
-		e.eventId = "qwertyuiop";
-		e.owner = "JinghanXu";
-		e.title = "Art Social";
-		e.description = "have some fun";
-		e.location = "San Francisco";
-		
-		return e;
+	public static Event buildEvent() {
+		return new Event();
+	}
+	
+	
+	public String getEventId() {
+		return eventId;
+	}
+
+
+	public Event setEventId(String eventId) {
+		this.eventId = eventId;
+		return this;
+	}
+
+
+	public EventType getType() {
+		return type;
+	}
+
+
+	public Event setType(EventType type) {
+		this.type = type;
+		return this;
+	}
+
+
+	public User getOwner() {
+		return owner;
+	}
+
+
+	public Event setOwner(User owner) {
+		this.owner = owner;
+		return this;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public Event setTitle(String title) {
+		this.title = title;
+		return this;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public Event setDescription(String description) {
+		this.description = description;
+		return this;
+	}
+
+
+	public String getLocation() {
+		return location;
+	}
+
+
+	public Event setLocation(String location) {
+		this.location = location;
+		return this;
+	}
+
+
+	public List<EventUser> getParticipants() {
+		return participants;
+	}
+
+
+	public Event setParticipants(List<EventUser> participants) {
+		this.participants = participants;
+		return this;
+	}
+
+
+	public enum EventType {
+		DINNER, MOVIE, TECH_TALK, BAR, SOCIAL, OTHER
 	}
 }
--- timeinsponge/src/main/java/com/timeinsponge/meeting/object/User.java
+++ timeinsponge/src/main/java/com/timeinsponge/meeting/object/User.java
@@ -1,5 +1,36 @@
 package com.timeinsponge.meeting.object;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class User {
 
+	public String userId;
+	public String username;
+	public String email;
+	public String timezone;
+	public Date last_login_time;
+	public Date last_update_time;
+	public Date created_time;
+
+	transient String password;
+	
+	public static User getOwner() {
+		User u = new User();
+		u.userId = "abcdefg";
+		u.username = "JinghanXu";
+		u.email = "jinghanx.com";
+		u.timezone = TimeZone.getDefault().getDisplayName();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.HOUR, -1);
+		u.last_login_time = cal.getTime();
+		cal.add(Calendar.DATE, -7);
+		u.last_update_time = cal.getTime();
+		cal.add(Calendar.MONTH, -1);
+		u.created_time = cal.getTime();
+		
+		return u;
+	}
 }
